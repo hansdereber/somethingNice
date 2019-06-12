3795d2e3ac256ccb253fe069d0b9bc2e30d14f1c
Christian Geisler <christian.geisler@posteo.de>
2019-06-12 22:37:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Snips-Witze
+Böse Witze. Sie sind weder jugendfrei noch politisch korrekt.
+
+"Wie bekommt man 11 Millionen Follower? Man läuft mit einer Wasserflasche durch Afrika." \
+"Was ist blau und orange und liegt auf dem Boden eines Pools? Ein Baby mit geplatzten Schwimmflügeln." \
+"Schwarzer Humor ist wie was zu Essen. Hat nicht jeder."
+
+### Quellen
+- https://www.witzepause.com
+- https://schlechtewitze.com
---
+++ action-tellJoke.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import random
+import os
+
+
+def subscribe_intent_callback(hermes, intent_message):
+    conf = None
+    action_wrapper(hermes, intent_message, conf)
+
+
+def random_line(afile):
+    """ See Waterman's Reservoir Algorithm """
+
+    line = next(afile)
+    for num, aline in enumerate(afile, 2):
+        if (random.randrange(num) == 0):
+            line = aline
+    return line
+
+
+def action_wrapper(hermes, intent_message, conf):
+    """ Write the body of the function that will be executed once the intent is recognized. 
+    In your scope, you have the following objects : 
+    - intent_message : an object that represents the recognized intent
+    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
+    - conf : a dictionary that holds the skills parameters you defined. 
+      To access global parameters use conf['global']['parameterName']. 
+      For end-user parameters use conf['secret']['parameterName'] 
+
+    Refer to the documentation for further details. 
+    """
+
+    file = open(os.path.dirname(os.path.realpath(__file__)) + "/witze.txt")
+    result_sentence = random_line(file)
+    file.close()
+
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
+
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("DANBER:tellJoke", subscribe_intent_callback) \
+            .start()
